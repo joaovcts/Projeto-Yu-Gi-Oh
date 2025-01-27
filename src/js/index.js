04aa3747266a1161f0ff0228ff69d06efb2574fa
João Victor da Silva <163600611+joaovcts@users.noreply.github.com>
2025-01-27 08:48:42 -0300

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -9,6 +9,20 @@ const btnVoltar = document.getElementById("btn-voltar"); //Criada uma variável 
 const cartoes = document.querySelectorAll(".cartao");
 let cartaoAtual = 0;
 
+cartoes.forEach(cartao => {
+    cartao.addEventListener("click", function() {
+        const cartaVirada = cartao.querySelector(".carta-virada");
+        // virar o cartão:
+        cartao.classList.toggle("virar");
+        // mostrar o fundo da carta
+        cartaVirada.classList.toggle("mostrar-fundo-carta");
+        
+        const descricao = cartao.querySelector(".descricao");
+        descricao.classList.toggle("esconder");
+    });
+}); /*Para cada cartão seleciona*/
+
+
 // Passo 2 - dar um jeito de identificar o clique do usuário na seta avançar:
 btnAvancar.addEventListener("click", function () {
     if (cartaoAtual === cartoes.length - 1) return;
